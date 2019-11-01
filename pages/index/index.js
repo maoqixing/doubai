237dcb1f7e27a3032f5653ff1f5259ae7db0e792
maoqixing <879652938@qq.com>
2019-11-01 10:01:05 +0800

    飞翔开发发_渲染电影

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,11 +4,50 @@ const app = getApp()
 
 Page({
   data: {
-    
+    movices:[],
+    tvs:[],
+    shows:[]
   },
  
   onLoad: function () {
-  
+     var that=this;
+     wx.request({
+       url: 'https://m.douban.com/rexxar/api/v2/subject_collection/movie_showing/items',
+       success(res){
+         console.log(res.data.subject_collection_items)
+         var movices = res.data.subject_collection_items;
+         console.log("movices",movices)
+         console.log("movices2", movices[0].cover.url)
+         that.setData({
+           movices:movices
+         })
+       }
+       
+     })
+     wx.request({
+       url: 'https://m.douban.com/rexxar/api/v2/subject_collection/tv_hot/items',
+       success(res) {
+         console.log(res.data.subject_collection_items)
+         var tvs = res.data.subject_collection_items;
+         console.log("tvs", tvs)
+         console.log("tvs", tvs[0].cover.url)
+         that.setData({
+           tvs: tvs
+         })
+       }
+     })
+     wx.request({
+       url: 'https://m.douban.com/rexxar/api/v2/subject_collection/tv_variety_show/items',
+       success(res) {
+         console.log(res.data.subject_collection_items)
+         var shows = res.data.subject_collection_items;
+         console.log("shows", shows)
+         console.log("shows", shows[0].cover.url)
+         that.setData({
+           shows: shows
+         })
+       }
+     })
   },
  
 
